1d5c6d2f72b5f0c87d27e8bf637e36a01cc3ee20
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-03-17 22:40:06 +0530

    saveEmployee method

--- employeems/src/main/java/com/example/employeems/service/EmployeeService.java
+++ employeems/src/main/java/com/example/employeems/service/EmployeeService.java
@@ -3,10 +3,12 @@ import com.example.employeems.dto.EmployeeDTO;
 import com.example.employeems.entity.Employee;
 import com.example.employeems.repo.EmployeeRepo;
 import com.example.employeems.util.VarList;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
+@Transactional
 public class EmployeeService {
     @Autowired
     private EmployeeRepo employeeRepo;
